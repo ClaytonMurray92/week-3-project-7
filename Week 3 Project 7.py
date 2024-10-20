a98518a1d9480a8994c176b0d2bfff2ccec47029
ClaytonMurray92 <clamurr@okstate.edu>
2024-10-20 16:47:23 -0400

    Add files via upload

---
+++ Week 3 Project 7.py
@@ -0,0 +1,6 @@
+minutesPerHour = 60
+hoursPerDay = 24
+daysPerYear = 365
+years = int(input("Enter the number of years: "))
+minutes = years * daysPerYear * hoursPerDay * minutesPerHour
+print("The number of minutes is: ", minutes)
